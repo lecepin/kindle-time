8c83d8e6ff275d0f5d21f939cced1f4f286906c5
lecepin <383810086@qq.com>
2023-01-15 14:55:49 +0800

    fix: kindle compatibility😰

--- index.js
+++ index.js
@@ -1,32 +1,80 @@
-const dIns = lunisolar();
-const urlQuery = new URLSearchParams(window.location.search);
-const config = {
-  fontSize: +(urlQuery.get("fs") || 7),
-  rotate: urlQuery.get("r"),
-};
 const domApp = document.querySelector(".app");
 const domTime = document.querySelector(".time");
 const domDate = document.querySelector(".date");
 const domCnDate = document.querySelector(".cn-date");
 
-const render = (dIns) => {
-  domDate.innerHTML =
-    dIns.format("YYYY.M.D") +
-    " " +
-    "星期" +
-    ["日", "一", "二", "三", "四", "五", "六"][dIns._date.getDay()];
+function geturl(url) {
+  const arr = url.split("?");
 
-  domTime.innerHTML = dIns.format("H:m");
-  domCnDate.innerHTML = dIns.format("lDlM cZ年");
-  console.log(dIns.format("H:m"));
+  if (!arr[1]) {
+    return {};
+  }
+
+  const res = arr[1].split("&");
+  const items = {};
+  for (let i = 0; i < res.length; i++) {
+    const a = res[i].split("=");
+    items[a[0]] = a[1];
+  }
+  return items;
+}
+function formatDate(date, fmt = "yyyy-MM-dd") {
+  if (!date) {
+    return "";
+  }
+  if (typeof date === "number" || typeof date === "string") {
+    date = new Date(Number(date));
+  }
+  const o = {
+    "M+": date.getMonth() + 1,
+    "d+": date.getDate(),
+    "h+": date.getHours(),
+    "m+": date.getMinutes(),
+    "s+": date.getSeconds(),
+    "q+": Math.floor((date.getMonth() + 3) / 3),
+    S: date.getMilliseconds(),
+  };
+  if (/(y+)/.test(fmt))
+    fmt = fmt.replace(
+      RegExp.$1,
+      `${date.getFullYear()}`.substr(4 - RegExp.$1.length)
+    );
+  for (const k in o)
+    if (new RegExp(`(${k})`).test(fmt))
+      fmt = fmt.replace(
+        RegExp.$1,
+        RegExp.$1.length === 1 ? o[k] : `00${o[k]}`.substr(`${o[k]}`.length)
+      );
+  return fmt;
+}
+function render() {
+  const date = new Date();
+  const dateText = `${formatDate(date, "yyyy.M.d")} ${
+    urlQuery.l == "en"
+      ? ["SUN", "MON", "TUES", "WED", "THUR", "FRI", "SAT"][date.getDay()]
+      : "星期" + ["日", "一", "二", "三", "四", "五", "六"][date.getDay()]
+  }`;
+  const timeText = formatDate(date, "h:m");
+
+  if (domDate.innerHTML != dateText) domDate.innerHTML = dateText;
+  if (domTime.innerHTML != timeText) domTime.innerHTML = timeText;
+}
+
+const urlQuery = geturl(location.href);
+const config = {
+  fontSize: +(urlQuery.fs || 7),
+  rotate: urlQuery.r,
+  lang: urlQuery.l,
 };
 
 domTime.style.fontSize = config.fontSize + "rem";
 domDate.style.fontSize = config.fontSize / 2.5 + "rem";
 domCnDate.style.fontSize = config.fontSize / 4 + "rem";
-domApp.style.cssText = `transform: rotate(${config.rotate || 0}deg)`;
+domApp.style.cssText = `-webkit-transform: rotate(${
+  config.rotate || 0
+}deg) translate3d(-50%,-50%,0)`;
 
-render(dIns);
+render();
 setInterval(() => {
-  render(dIns);
+  render();
 }, 1000);
