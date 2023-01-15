2aa8f21980ffa8a3613c86a423ddc8ec5313e5b6
lecepin <383810086@qq.com>
2023-01-15 18:26:31 +0800

    feat: 农历实现

--- index.js
+++ index.js
@@ -48,21 +48,29 @@ function formatDate(date, fmt = "yyyy-MM-dd") {
   return fmt;
 }
 function render() {
+  // 保证 中国时区显示时间。不设置在一些 kindle 会出现时区问题
   const time = new Date();
   const len = time.getTime();
   const offset = time.getTimezoneOffset() * 60000; //本地时间与GMT时间差值
   const utcTime = len + offset; //格林尼治时间
   const date = new Date(utcTime + 3600000 * 8);
 
+  const lunar = calendar.solar2lunar(
+    date.getUTCFullYear(),
+    date.getUTCMonth() + 1,
+    date.getUTCDate()
+  );
   const dateText = `${formatDate(date, "yyyy.M.d")} ${
     urlQuery.l == "en"
       ? ["SUN", "MON", "TUES", "WED", "THUR", "FRI", "SAT"][date.getDay()]
       : "星期" + ["日", "一", "二", "三", "四", "五", "六"][date.getDay()]
   }`;
   const timeText = `${date.getHours()}:${date.getMinutes()}`;
+  const cnDateText = `${lunar.IMonthCn}${lunar.IDayCn} ${lunar.Animal}年`;
 
   if (domDate.innerHTML != dateText) domDate.innerHTML = dateText;
   if (domTime.innerHTML != timeText) domTime.innerHTML = timeText;
+  if (domCnDate.innerHTML != cnDateText) domCnDate.innerHTML = cnDateText;
 }
 
 const urlQuery = geturl(location.href);
